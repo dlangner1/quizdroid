63808aa9f6b60dd471088915269e76dee2d921f5
Dustin Langner <dustin.langner1@gmail.com>
2019-04-29 12:42:17 -0700

    Add recycler view

--- app/src/main/java/edu/us/ischool/dlangner/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/us/ischool/dlangner/quizdroid/MainActivity.kt
@@ -2,11 +2,18 @@ package edu.us.ischool.dlangner.quizdroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        
+
     }
 }
